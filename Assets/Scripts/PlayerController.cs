66cf0ce190ee236826a2c59da63d0896eb5cf83e
MJ Choi <1621139@gmail.com>
2021-04-27 03:28:09 +0900

    [UPDATE] reset before info

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody2D rigidbody;
+    private Rigidbody2D rigidbody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
 
     void Start()
     {
@@ -14,6 +22,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
         //if (Input.GetKey(KeyCode.D))
         if (Input.GetAxisRaw("Horizontal") > 0f)
         {
@@ -23,6 +33,15 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetAxisRaw("Horizontal") < 0f)
         {
             rigidbody.velocity = new Vector3(-moveSpeed, rigidbody.velocity.y, 0f);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0f, rigidbody.velocity.y, 0f);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed, 0f);
         }
     }
 }
