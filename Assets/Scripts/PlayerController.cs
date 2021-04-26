497aed2f922729c8185d181d19ef40555c0d1404
MJ Choi <1621139@gmail.com>
2021-04-27 02:44:58 +0900

    [ADD] move as keyword position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{    
+{
+    public float moveSpeed;
+    public Rigidbody2D rigidbody;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
- 
+
     void Update()
     {
-        
+        //if (Input.GetKey(KeyCode.D))
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rigidbody.velocity = new Vector3(moveSpeed, rigidbody.velocity.y, 0f);
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rigidbody.velocity = new Vector3(-moveSpeed, rigidbody.velocity.y, 0f);
+        }
     }
 }
