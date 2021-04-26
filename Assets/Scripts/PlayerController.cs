b09ecc9502a1ba0ff891ce2401350d7422daf9a8
MJ Choi <1621139@gmail.com>
2021-04-27 03:22:11 +0900

    [UPDATE] limited up-position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{    
+{
+    public float moveSpeed;
+    private Rigidbody2D rigidbody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
- 
+
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        //if (Input.GetKey(KeyCode.D))
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rigidbody.velocity = new Vector3(moveSpeed, rigidbody.velocity.y, 0f);
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rigidbody.velocity = new Vector3(-moveSpeed, rigidbody.velocity.y, 0f);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector3(0f, rigidbody.velocity.y, 0f);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpSpeed, 0f);
+        }
     }
 }
